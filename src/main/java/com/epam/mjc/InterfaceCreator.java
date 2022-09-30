45b932aac7a9bb75006c3284e3ad5ac0369d93cd
Aliaksandr_Palavinkin <Aliaksandr_Palavinkin@epam.com>
2022-09-30 15:06:27 +0400

    task created

---
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -0,0 +1,8 @@
+package com.epam.mjc;
+
+public class InterfaceCreator {
+
+    public Operation<Integer> divideBy(Integer divider) {
+        throw new UnsupportedOperationException("You should implement this method.");
+    }
+}
