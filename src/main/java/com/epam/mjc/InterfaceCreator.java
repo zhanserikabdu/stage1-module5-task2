6bc2f1209e4c70c5c5930368073a864b7392bc7f
NANO <File changed>
2022-11-29 13:09:55 +0600

    1 submission

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,8 +1,18 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InterfaceCreator {
 
     public Operation<Integer> divideBy(Integer divider) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        Operation<Integer> operation = arg -> {
+            List<Integer> list = new ArrayList<>();
+            for (Integer i : arg) {
+                list.add(i / divider);
+            }
+            return list;
+        };
+        return operation;
     }
 }
